2f88fe7d68fbbbe8d29322df89fc2e621542ddd8
Paola Franco <paoelizabeth69@gmail.com>
2020-06-23 22:25:09 -0500

    Deber

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿
+using System;
+
+namespace ConsolaTerceroA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            if (args.Length > 0)
+            {
+                Console.WriteLine(args[0]);
+            }
+
+            Console.Write("Bienvenidos,");
+            Console.WriteLine(" ingrese sus nombres y apellidos");
+            string nombresIngresados = Console.ReadLine();
+            Console.Write("Porfavor,");
+            Console.WriteLine(" ingrese su numero de cedula");
+             string nombresIngresados1 = Console.ReadLine();
+            Console.WriteLine("Muchas gracias ya te encuetras registrado: " + nombresIngresados);
+
+
+            Console.Read();
+            Console.ReadLine();
+        }
+    }
+}
