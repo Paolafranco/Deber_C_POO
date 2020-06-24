a8043d27ec7859595c6c4cd3a215c4357abb8794
Paolafranco <55416764+Paolafranco@users.noreply.github.com>
2020-06-24 15:52:32 -0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -19,6 +19,21 @@ namespace ConsolaTerceroA
             Console.Write("Porfavor,");
             Console.WriteLine(" ingrese su numero de cedula");
              string nombresIngresados1 = Console.ReadLine();
+             Console.Write("Porfavor,");
+            Console.WriteLine(" ingrese su numero celular");
+            string nombresIngresados2 = Console.ReadLine();
+            Console.Write("Porfavor,");
+            Console.WriteLine(" ingrese su estado civil");
+            string nombresIngresados3 = Console.ReadLine();
+            Console.Write("Porfavor,");
+            Console.WriteLine(" ingrese la fecha de nacimiento");
+            string nombresIngresados4 = Console.ReadLine();
+            Console.Write("Porfavor,");
+            Console.WriteLine(" ingrese su nacionalidad");
+            string nombresIngresados5 = Console.ReadLine();
+            Console.Write("Porfavor,");
+            Console.WriteLine(" ingrese su lugar de nacimiento?");
+            string nombresIngresados6 = Console.ReadLine();
             Console.WriteLine("Muchas gracias ya te encuetras registrado: " + nombresIngresados);
 
 
